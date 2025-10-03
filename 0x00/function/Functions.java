ab6730a24f24c6ba42e1750d74b8bbbe59e72607
Valdizar Paula de Andrade Junior <zerdax.sjc@gmail.com>
2025-10-03 17:47:57 -0300

    [Projeto 0x00/nomenclatura - Projeto 0x00/function] - Implementação 1

--- 0x00/function/Functions.java
+++ 0x00/function/Functions.java
@@ -1,10 +1,5 @@
 public class Functions {
     // Faça somente 1 coisa
-    //A
-//    public void saveCash(Income income, Expense expense){
-//        inRepo.save(income);
-//        outRepo.save(expense);
-//    }
     //B
     public void saveIncome(Income income){
         inRepo.save(income);
@@ -19,27 +14,12 @@ public class Functions {
     public User saveUser(User user){
         return userService.save(user);
     }
-    //B
-//    @PostMapping("/user")
-//    public User saveUser(User user){
-//        return userService.userRepository.save(user);
-//    }
 
     // Arguments
     //A
     public void saveUser(User user){};
-    //B
-//    public void saveUser(String userName, User user, Date date, boolean isAdmin){};
 
     //  No side effects
-    //A
-//    public User saveUser(User user){
-//        if(user.isAdmin){
-//            user.setRole(user);
-//        }
-//        return userRepository.save(user);
-//    }
-
     //B
     public User saveUser(User user){
         return userRepository.save(user);
@@ -49,15 +29,6 @@ public class Functions {
     }
 
     // Evite duplicados, extraindo para novos métodos
-    //A
-//    public Environment getEnvironment(){
-//        String version = versionService.getVersion();
-//        return environmentService.getEnvironment(version);
-//    }
-//    public Release getRelease(){
-//        String version = versionService.getVersion();
-//        return releaseService.getRelease(version);
-//    }
     //B
     public Environment getEnvironment(){
         return environmentService.getEnvironment(getVersion());
